8854fe6fab5736a454be47d4914d0425ce61c25e
niki4smirn <kitos.smirnov@gmail.com>
2022-02-28 18:52:15 +0300

    Add Positivity, CheckOverflow, Function implementation and tests for them

--- lab2/solution.asm
+++ lab2/solution.asm
@@ -9,17 +9,144 @@
 
                     section .data
 
-                    ; YOUR_CODE_HERE
+positive_number_response:
+                    db 'Yeah) It', 39, 's positive :D', 0
+negative_number_response:
+                    db 'Ohh.. so much negative :(', 0
 
                     section .text
 
-Positivity:         ; YOUR_CODE_HERE
+Positivity:         ; x in rdi
+                    xor rax, rax
+                    cmp rdi, 0
+                    je .negative
+                    shr rdi, 63
+                    cmp rdi, 1
+                    je .negative
+                    jmp .positive
+.positive:
+                    mov rax, positive_number_response
+                    jmp .return
+.negative:
+                    mov rax, negative_number_response
+.return:
                     ret
 
-CheckOverflow:      ; YOUR_CODE_HERE
+CheckOverflow:      ; x in rdi, y in rsi
+                    mov r8, 0
+                    mov rcx, 0
+                    mov rax, rdi
+
+                    ; x^2
+                    imul rdi
+                    seto cl
+                    cmp cl, 1
+                    je .overflow
+                    mov r9, rax
+                    ; x^2 in r9
+
+                    mov rax, rsi
+                    imul rdi
+                    add rax, rax
+                    ; 2xy in rax
+
+                    mov r10, rax
+                    add r10, r9
+                    ; x^2 + 2xy in r10
+
+                    mov rax, rsi
+                    ; y^2
+                    imul rsi
+                    seto cl
+                    cmp cl, 1
+                    je .overflow
+
+                    add rax, r10
+                    seto cl
+                    cmp cl, 1
+                    je .overflow
+
+                    cmp rax, 0
+                    je .overflow
+
+                    mov rax, r9
+                    ; x^3
+                    imul rdi
+                    seto cl
+                    cmp cl, 1
+                    je .overflow
+
+                    ; x^4
+                    imul rdi
+                    seto cl
+                    cmp cl, 1
+                    je .overflow
+                    xor rax, rax
+                    jmp .return
+.overflow:
+                    mov rax, 1
+.return:
                     ret
 
-Function:           ; YOUR_CODE_HERE
+Function:           ; x in rdi
+                    cmp rdi, -1
+                    jl .first_branch
+
+                    cmp rdi, 17
+                    jle .second_branch
+
+                    jmp .third_branch
+.first_branch:
+                    mov rax, rdi
+                    imul rax
+                    add rax, rax
+                    sub rax, 3
+                    jmp .return
+.second_branch:
+                    mov r8, 10
+                    sub r8, rdi
+                    sub r8, rdi
+                    sub r8, rdi
+                    ; 10 - 3x in r8
+
+                    mov r10, r8
+                    ; in r10 stores value to make ceil
+                    cmp r10, 0
+                    jge .skip_abs
+                    neg r10
+.skip_abs:
+                    dec r10
+
+                    mov rax, rdi
+                    imul rax
+                    mov r9, 36
+                    sub r9, rax
+                    ; 36 - x^2 in r9
+
+                    cmp r9, 0
+                    jl .sub
+                    jmp .add
+.sub:
+                    sub r9, r10
+                    jmp .end_if
+.add:
+                    add r9, r10
+.end_if:
+                    mov rax, r9
+                    cqo
+                    idiv r8
+
+                    jmp .return
+.third_branch:
+                    mov rax, rdi
+                    imul rdi
+                    imul rdi
+
+                    add rax, 2
+                    neg rax
+
+                    jmp .return
+.return:
                     ret
 
 IsInCircle:         ; YOUR_CODE_HERE
--- lab2/tests.cpp
+++ lab2/tests.cpp
@@ -11,6 +11,159 @@ extern "C" int8_t MagicMetric(int64_t x);
 
 // ---------------------------------------------------------
 
-// YOUR_TESTS_HERE
+#include <random>
+
+class RandomGenerator {
+ public:
+  RandomGenerator() = default;
+  RandomGenerator(int64_t min_value, int64_t max_value);
+
+  int64_t GetValue();
+
+ private:
+  std::mt19937 gen{std::random_device()()};
+  std::uniform_int_distribution<int64_t> distribution;
+};
+
+RandomGenerator::RandomGenerator(int64_t min_value, int64_t max_value) :
+    distribution(std::uniform_int_distribution<int64_t>(min_value,
+                                                        max_value)) {}
+
+int64_t RandomGenerator::GetValue() {
+  return distribution(gen);
+}
+
+namespace positivity_responses {
+const char* positive = "Yeah) It's positive :D";
+const char* negative = "Ohh.. so much negative :(";
+}
+
+TEST(Positivity, Simple) {
+  EXPECT_STREQ(Positivity(1), positivity_responses::positive);
+  EXPECT_STREQ(Positivity(-1), positivity_responses::negative);
+  EXPECT_STREQ(Positivity(0), positivity_responses::negative);
+}
+
+TEST(Positivity, Generator) {
+  RandomGenerator gen(std::numeric_limits<int64_t>::min(),
+                      std::numeric_limits<int64_t>::max());
+  const int kTestsCount = 1e5;
+  for (int _ = 0; _ < kTestsCount; ++_) {
+    int64_t x = gen.GetValue();
+    if (x > 0) {
+      EXPECT_STREQ(Positivity(x), positivity_responses::positive);
+    } else {
+      EXPECT_STREQ(Positivity(x), positivity_responses::negative);
+    }
+  }
+}
+
+// ---------------------------------------------------------
+
+namespace overflow {
+namespace edge {
+const int kFourth = 55108;
+const int64_t kSecond = 3037000499;
+}
+namespace min_over {
+const int kFourth = edge::kFourth + 1;
+const int64_t kSecond = edge::kSecond + 1;
+}
+}
+
+bool IsInBounds(int64_t value, int64_t left_bound, int64_t right_bound) {
+  return left_bound <= value && value <= right_bound;
+}
+
+TEST(CheckOverflow, XFourthPower) {
+  EXPECT_EQ(CheckOverflow(overflow::edge::kFourth, 0), 0);
+  EXPECT_EQ(CheckOverflow(overflow::min_over::kFourth, 0), 1);
+  EXPECT_EQ(CheckOverflow(-overflow::edge::kFourth, 0), 0);
+  EXPECT_EQ(CheckOverflow(-overflow::min_over::kFourth, 0), 1);
+  EXPECT_EQ(CheckOverflow(100000, 0), 1);
+  EXPECT_EQ(CheckOverflow(std::numeric_limits<int64_t>::min(), 0), 1);
+  EXPECT_EQ(CheckOverflow(std::numeric_limits<int64_t>::max(), 0), 1);
+}
+
+TEST(CheckOverflow, YSecondPower) {
+  EXPECT_EQ(CheckOverflow(0, overflow::edge::kSecond), 0);
+  EXPECT_EQ(CheckOverflow(0, overflow::min_over::kSecond), 1);
+  EXPECT_EQ(CheckOverflow(0, -overflow::edge::kSecond), 0);
+  EXPECT_EQ(CheckOverflow(0, -overflow::min_over::kSecond), 1);
+  EXPECT_EQ(CheckOverflow(0, std::numeric_limits<int64_t>::min()), 1);
+  EXPECT_EQ(CheckOverflow(0, std::numeric_limits<int64_t>::max()), 1);
+}
+
+TEST(CheckOverflow, SquareExpression) {
+  ASSERT_EQ(CheckOverflow(overflow::edge::kFourth,
+                          overflow::edge::kSecond), 1);
+}
+
+void OverflowGeneratorTest(int64_t min_val, int64_t max_val) {
+  RandomGenerator gen(min_val, max_val);
+  const int kTestsCount = 1e7;
+  for (int _ = 0; _ < kTestsCount; ++_) {
+    int64_t x = gen.GetValue();
+    int64_t y = gen.GetValue();
+    bool ans = false;
+    ans |= !IsInBounds(x, -overflow::edge::kFourth, overflow::edge::kFourth);
+    ans |= !IsInBounds(y, -overflow::edge::kSecond, overflow::edge::kSecond);
+    if (!ans && x + y != 0) {
+      ASSERT_FALSE(CheckOverflow(x, y))  << x << ' ' << y;
+    }
+    if (x + y == 0) {
+      ASSERT_TRUE(CheckOverflow(x, y))  << x << ' ' << y;
+    }
+  }
+}
+
+TEST(CheckOverflow, SmallGenerator) {
+  const int kAbsLimit = 2e5;
+  OverflowGeneratorTest(-kAbsLimit, kAbsLimit);
+
+}
+
+TEST(CheckOverflow, Generator32) {
+  OverflowGeneratorTest(std::numeric_limits<int32_t>::min(),
+                        std::numeric_limits<int32_t>::max());
+}
+
+TEST(CheckOverflow, Generator64) {
+  OverflowGeneratorTest(std::numeric_limits<int64_t>::min(),
+                        std::numeric_limits<int64_t>::max());
+}
+
+// ---------------------------------------------------------
+
+TEST(Function, Simple) {
+  ASSERT_EQ(Function(-2), 5);
+  ASSERT_EQ(Function(0), 4);
+  ASSERT_EQ(Function(1), 5);
+  ASSERT_EQ(Function(4), -10);
+  ASSERT_EQ(Function(18), -5834);
+}
+
+TEST(Function, Generator) {
+  const int kAbsLimit = 2e5;
+  RandomGenerator gen(-kAbsLimit, kAbsLimit);
+  const int kTestsCount = 1e7;
+  for (int _ = 0; _ < kTestsCount; ++_) {
+    int64_t x = gen.GetValue();
+    int64_t res;
+    if (x < -1) {
+      res = 2 * x * x - 3;
+    } else if (x <= 17) {
+      double val = (36 - x * x) * 1.0 / (10 - 3 * x);
+      if (val < 0) {
+        res = std::floor(val);
+      } else {
+        res = std::ceil(val);
+      }
+    } else {
+      res = - x * x * x - 2;
+    }
+    ASSERT_EQ(Function(x), res) << x;
+  }
+}
 
 // ---------------------------------------------------------
