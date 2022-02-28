cd072e003d0faf4b5e8ed6e408a94a7dbeb04727
niki4smirn <kitos.smirnov@gmail.com>
2022-02-28 08:39:38 +0300

    Add lab2 template

---
+++ lab2/solution.asm
@@ -0,0 +1,38 @@
+                    global Positivity
+                    global CheckOverflow
+                    global Function
+                    global IsInCircle
+                    global TwoIntegers
+                    global CountNumbers
+                    global Switch
+                    global MagicMetric
+
+                    section .data
+
+                    ; YOUR_CODE_HERE
+
+                    section .text
+
+Positivity:         ; YOUR_CODE_HERE
+                    ret
+
+CheckOverflow:      ; YOUR_CODE_HERE
+                    ret
+
+Function:           ; YOUR_CODE_HERE
+                    ret
+
+IsInCircle:         ; YOUR_CODE_HERE
+                    ret
+
+TwoIntegers:        ; YOUR_CODE_HERE
+                    ret
+
+CountNumbers:       ; YOUR_CODE_HERE
+                    ret
+
+Switch:             ; YOUR_CODE_HERE
+                    ret
+
+MagicMetric:        ; YOUR_CODE_HERE
+                    ret
---
+++ lab2/tests.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+extern "C" const char* Positivity(int64_t x);
+extern "C" bool CheckOverflow(int64_t x, int64_t y);
+extern "C" int64_t Function(int64_t x);
+extern "C" bool IsInCircle(int64_t x, int64_t y, int64_t d);
+extern "C" bool TwoIntegers(int64_t x, int64_t y);
+extern "C" int8_t CountNumbers(int64_t a, int64_t b, int64_t c);
+extern "C" const char* Switch(int64_t k);
+extern "C" int8_t MagicMetric(int64_t x);
+
+// ---------------------------------------------------------
+
+// YOUR_TESTS_HERE
+
+// ---------------------------------------------------------
