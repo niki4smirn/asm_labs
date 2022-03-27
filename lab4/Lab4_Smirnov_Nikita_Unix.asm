9f7814f946be3dd161bf5f030e967342210b6008
niki4smirn <kitos.smirnov@gmail.com>
2022-03-27 11:02:14 +0300

    Add Product implementation and tests

--- lab4/Lab4_Smirnov_Nikita_Unix.asm
+++ lab4/Lab4_Smirnov_Nikita_Unix.asm
@@ -2,6 +2,30 @@
 
                     section .text
 
-AsmProduct:
+AsmProduct:         ; &array in rdi
+                    movsx rsi, esi
+                    ; size in rsi
+                    movsx rcx, edx
+                    ; module in ecx
+                    ; ans in rax
+                    mov rax, 1
+.loop_begin:
+                    dec rsi
+                    ; &cur_element in r8
+                    lea r8, [rdi + 4 * rsi]
+                    movsx r9, DWORD [r8]
+                    imul r9
+                    cqo
+                    idiv rcx
+                    mov rax, rdx
+
+                    cmp rax, 0
+                    jnl .after_add
+
+                    add rax, rcx
+
+.after_add:
+                    cmp rsi, 0
+                    jg .loop_begin
 
                     ret
--- lab4/tests.cpp
+++ lab4/tests.cpp
@@ -8,22 +8,96 @@ extern "C" int32_t AsmProduct(
 #include <algorithm>
 #include <random>
 
+template<typename T>
 class RandomGenerator {
  public:
   RandomGenerator() = default;
-  RandomGenerator(uint64_t min_value, uint64_t max_value);
+  RandomGenerator(T min_value, T max_value);
 
-  uint64_t GetValue();
+  T GetValue();
 
  private:
   std::mt19937 gen{std::random_device()()};
-  std::uniform_int_distribution<uint64_t> distribution;
+  std::uniform_int_distribution<T> distribution;
 };
 
-RandomGenerator::RandomGenerator(uint64_t min_value, uint64_t max_value) :
-    distribution(std::uniform_int_distribution<uint64_t>(min_value,
-                                                         max_value)) {}
+template<typename T>
+RandomGenerator<T>::RandomGenerator(T min_value, T max_value) :
+    distribution(std::uniform_int_distribution<T>(min_value, max_value)) {}
 
-uint64_t RandomGenerator::GetValue() {
+template<typename T>
+T RandomGenerator<T>::GetValue() {
   return distribution(gen);
+}
+
+// ---------------------------------------------------------
+
+int32_t RightProduct(
+    const int32_t* array, int32_t size, int32_t module) {
+  int64_t ans = 1;
+  for (int i = 0; i < size; ++i) {
+    ans *= array[i];
+    ans %= module;
+    ans += module;
+    ans %= module;
+  }
+  return ans;
+}
+
+TEST(AsmProduct, Simple) {
+  {
+    auto* array = new int32_t[]{1, 2, 3, 42};
+    ASSERT_EQ(AsmProduct(array, 4, 2), 0);
+    delete[] array;
+  }
+  {
+    auto* array = new int32_t[]{1, 3, 3, 49};
+    ASSERT_EQ(AsmProduct(array, 4, 2), 1);
+    delete[] array;
+  }
+  {
+    auto* array = new int32_t[]{1, 2, 3, 49};
+    ASSERT_EQ(AsmProduct(array, 4, 15), 9);
+    delete[] array;
+  }
+  {
+    auto* array = new int32_t[]{1, 2, 3, 0, 49, 215125, 1436};
+    ASSERT_EQ(AsmProduct(array, 7, 2364), 0);
+    delete[] array;
+  }
+  {
+    auto* array = new int32_t[]{1, -2, 4, -5, 6, -15};
+    ASSERT_EQ(AsmProduct(array, 6, 17), 4);
+    delete[] array;
+  }
+}
+
+std::string ArrToStr(const int32_t* array, int32_t size) {
+  std::string result;
+  for (int i = 0; i < size; ++i) {
+    result += std::to_string(array[i]);
+    if (i + 1 != size) {
+      result += ", ";
+    }
+  }
+  return result;
+}
+
+TEST(AsmProduct, Generator) {
+  const int kTestsCount = 1e4;
+  RandomGenerator<int32_t> size_gen{1, 1000};
+  for (int _ = 0; _ < kTestsCount; ++_) {
+    int32_t size = size_gen.GetValue();
+    auto* array = new int32_t[size];
+    int32_t bound_value = std::numeric_limits<int32_t>::max();
+    RandomGenerator<int32_t> array_gen{-bound_value, bound_value};
+    for (int i = 0; i < size; ++i) {
+      array[i] = array_gen.GetValue();
+    }
+    RandomGenerator<int32_t> module_generator{2, (1 << 30)};
+    int32_t module = module_generator.GetValue();
+    ASSERT_EQ(AsmProduct(array, size, module),
+              RightProduct(array, size, module)) << ArrToStr(array, size)
+                                                 << ' ' << module;
+  }
 }
